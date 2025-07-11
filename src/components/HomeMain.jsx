9d96f246524aef09d96bd579b8cdb7ab68393239
Reuben-24 <reubengfaltiska@gmail.com>
2025-07-12 01:57:45 +1000

    Add Header and Footer components

---
+++ src/components/HomeMain.jsx
@@ -0,0 +1,19 @@
+function HomeMain() {
+  return (
+    <>
+      <section class="hero">
+        <h1>Step Into Style</h1>
+        <p>Explore the latest sneakers in our 2025 collection.</p>
+        <button>Shop Now</button>
+      </section>
+      <secton>
+
+      </secton>
+      <secton>
+        
+      </secton>
+    </>
+  );
+}
+
+export default HomeMain;
